e49c72fc847c21ac4b1181f943bc264da9f4dd2d
rajkumar060301 <rajkumarktr10@gmail.com>
2026-06-13 14:25:04 +0530

    testing api controller checking cicd

--- src/main/java/com/moov/niger/cciportal/controller/TestController.java
+++ src/main/java/com/moov/niger/cciportal/controller/TestController.java
@@ -16,7 +16,7 @@ public class TestController {
 
     @GetMapping("/test")
     public  String test(){
-        return "testing api";
+        return "testing api controller checking cicd";
 
     }
 
