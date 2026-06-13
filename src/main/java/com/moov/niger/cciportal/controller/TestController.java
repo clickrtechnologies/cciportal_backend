50dd91e6f1cc104b945d40d33e162e623dbcd257
rajkumar060301 <rajkumarktr10@gmail.com>
2026-06-13 14:10:24 +0530

    updated test controller

---
+++ src/main/java/com/moov/niger/cciportal/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.moov.niger.cciportal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * Author Name: Raj Kumar
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * JDK: 17 version
+ * Date: 13-06-2026
+ */
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping("/test")
+    public  String test(){
+        return "testing api";
+    }
+
+
+}
