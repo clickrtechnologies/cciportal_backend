8517f7c16e21bb7cf62882d1b15ece9e12c241bf
rajkumar060301 <rajkumarktr10@gmail.com>
2026-06-17 00:49:53 +0530

    updated security chain

--- src/main/java/com/moov/niger/cciportal/config/SecurityConfig.java
+++ src/main/java/com/moov/niger/cciportal/config/SecurityConfig.java
@@ -5,21 +5,44 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)
-            throws Exception {
+    public CorsConfigurationSource corsConfigurationSource() {
 
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                )
-                .httpBasic(Customizer.withDefaults());
+        CorsConfiguration config = new CorsConfiguration();
 
-        return http.build();
+        config.setAllowedOrigins(List.of(
+                "http://cci.kristacrbt.in",
+                "http://localhost:4200",
+                "http://localhost:8089",
+                "http://127.0.0.1:4200"
+        ));
+
+        config.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
+        ));
+
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }
